f3ea922406f31f86302268f54733a1b5b85b4107
ZlatkoMarianov <zlatkomarianov@abv.bg>
2025-12-03 20:44:03 +0200

    Add useRequest hook for HTTP requests

---
+++ client/src/hooks/useRequest.js
@@ -0,0 +1,20 @@
+
+const baseUrl = 'http://localhost:3030';
+
+export default function useRequest() {
+
+
+    const request = async (url, method, data, extraHeaders = {}) => {
+        const options = {
+            method,
+            headers: {
+                ...extraHeaders,
+            }
+        };
+
+        if (data) {
+            options.headers["content-type"] = "application/json";
+            options.body = JSON.stringify(data);
+        }
+    };
+}
