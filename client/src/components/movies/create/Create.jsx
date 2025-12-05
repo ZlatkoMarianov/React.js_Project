9635199cf596e4ed696a828514f45c5b54afb3f5
ZlatkoMarianov <zlatkomarianov@abv.bg>
2025-12-05 22:16:56 +0200

    Refactor movie creation form to use custom hooks

--- client/src/components/movies/create/Create.jsx
+++ client/src/components/movies/create/Create.jsx
@@ -1,7 +1,36 @@
-import MovieForm from "../movieForm/MovieForm.jsx";
+import { useNavigate } from 'react-router';
+import useMovieService from '../../../hooks/useMovieService.js';
+import MovieForm from '../movieForm/MovieForm.jsx';
+import useForm from '../../../hooks/useForm.js';
+
+const initialValues = {
+  title: '',
+  year: '',
+  genre: '',
+  imageUrl: '',
+  description: '',
+  rating: '',
+};
 
 export default function Create() {
-    return (
-        <MovieForm title="Create Movie" />
-    );
+  const { create } = useMovieService();
+  const navigate = useNavigate();
+
+  const submitHandler = async (values) => {
+    await create(values);
+
+    navigate('/catalog');
+  };
+
+  const { register, formAction } = useForm( initialValues, submitHandler);
+
+  return (
+    <MovieForm
+        formTitle="Create Movie"
+        register={register}
+        formAction={formAction}
+        submitButton="Create" 
+        onCancel={() => navigate('/catalog')}
+    />
+  );
 }
--- client/src/components/movies/movieForm/MovieForm.jsx
+++ client/src/components/movies/movieForm/MovieForm.jsx
@@ -1,42 +1,48 @@
 import styles from './MovieForm.module.css';
 
-export default function MovieForm({ title }) {
+export default function MovieForm({ formTitle, register, formAction, submitButton = 'Save', onCancel = null }) {
   return (
     <section className={`section ${styles.movieFormPage}`}>
       <div className={styles.formCard}>
-        <h2 className={styles.formTitle}>{title}</h2>
-        <form className={styles.movieForm}>
+        <h2 className={styles.formTitle}>{formTitle}</h2>
+        <form className={styles.movieForm} action={formAction}>
           <label className={styles.formField}>
             <span>Title</span>
-            <input type="text" className="input" placeholder="Movie title" />
+            <input type="text" className="input" placeholder="Movie title" {...register('title')} />
           </label>
           <label className={styles.formField}>
             <span>Year</span>
-            <input type="number" className="input" placeholder={2024} />
+            <input type="number" className="input" placeholder={2024} {...register('year')} />
           </label>
           <label className={styles.formField}>
             <span>Genre</span>
-            <input type="text" className="input" placeholder="Sci-Fi, Drama, Action..." />
+            <input type="text" className="input" placeholder="Sci-Fi, Drama, Action..." {...register('genre')} />
+          </label>
+          <label className={styles.formField}>
+            <span>Duration (minutes)</span>
+            <input type="number" className="input" placeholder="120" {...register('duration')} />
+          </label>
+          <label className={styles.formField}>
+            <span>Rating (1-5)</span>
+            <input type="number" className="input" min={1} max={5} step="0.1" placeholder="4.5" {...register('rating')} />
           </label>
           <label className={styles.formField}>
             <span>Image URL</span>
-            <input type="text" className="input" placeholder="https://..." />
+            <input type="text" className="input" placeholder="https://..." {...register('imageUrl')} />
           </label>
           <label className={styles.formField}>
             <span>Description</span>
-            <textarea className="input textarea" rows={5} placeholder="Short summary of the movie..." defaultValue={''} />
-          </label>
-          <label className={styles.formField}>
-            <span>Rating (1–5)</span>
-            <input type="number" className="input" min={1} max={5} step="0.1" placeholder="4.5" />
+            <textarea className="input textarea" rows={5} placeholder="Short summary of the movie..." {...register('description')} />
           </label>
           <div className={styles.formActions}>
             <button className="btn btn-primary" type="submit">
-              Save
+              {submitButton}
             </button>
-            <button className="btn btn-outline" type="button">
-              Cancel
-            </button>
+            {onCancel && (
+              <button className="btn btn-outline" type="button" onClick={onCancel}>
+                Cancel
+              </button>
+            )}
           </div>
         </form>
       </div>
