5aad5d2dc0c066278fa5e2cda7b8045a7603fac7
ZlatkoMarianov <zlatkomarianov@abv.bg>
2025-12-07 20:15:20 +0200

    Add form validation to movie Edit

--- client/src/components/movies/edit/EditForm.jsx
+++ client/src/components/movies/edit/EditForm.jsx
@@ -1,10 +1,13 @@
 import { useNavigate } from 'react-router';
 import useForm from '../../../hooks/useForm.js';
 import MovieForm from '../movieForm/MovieForm.jsx';
+import useValidation from '../../../hooks/useValidation.js';
 
 export default function EditForm({ movie, editMovieHandler }) {
   const navigate = useNavigate();
-  const { register, formAction } = useForm(movie, editMovieHandler);
+  const { validateMovie } = useValidation();
+
+  const { register, formAction, errors, touched } = useForm(movie, editMovieHandler, validateMovie);
 
   return (
     <MovieForm
@@ -13,6 +16,8 @@ export default function EditForm({ movie, editMovieHandler }) {
       formAction={formAction}
       submitButton="Update"
       onCancel={() => navigate(`/movies/${movie._id}/details`)}
+      errors={errors}
+      touched={touched}
     />
   );
 }
