4f53ec5b1c7091e77cd62141df2a9a55fcb12082
ZlatkoMarianov <zlatkomarianov@abv.bg>
2025-12-08 20:46:02 +0200

    Add loading spinner to MoviesCatalog

--- client/src/components/movies/moviesCatalog/MoviesCatalog.jsx
+++ client/src/components/movies/moviesCatalog/MoviesCatalog.jsx
@@ -2,32 +2,31 @@ import { useEffect, useState } from 'react';
 import MovieCard from '../movieCard/MovieCard.jsx';
 import styles from './MoviesCatalog.module.css';
 import useMovieService from '../../../hooks/useMovieService.js';
+import Spinner from '../../common/Spinner.jsx';
 
 export default function MoviesCatalog() {
   const [movies, setMovies] = useState([]);
+  const [Loading, setLoading] = useState(true);
   const { getAll } = useMovieService();
 
   useEffect(() => {
-    getAll().then((result) => setMovies(result));
+    setLoading(true);
+    getAll()
+      .then((result) => setMovies(result))
+      .catch((err) => alert(err.message))
+      .finally(() => setLoading(false));
   }, []);
+
+  if (Loading) {
+    return <Spinner />;
+  }
 
   return (
     <section className={`section ${styles.catalogPage}`}>
       <div className={styles.sectionHeader}>
         <h2>Movies Catalog</h2>
-
-        {/* // TODO BONUS */}
-        {/* <div className={styles.sectionActions}>
-          <input type="text" className={`input ${styles.searchInput}`} placeholder="Search by title (local filter)..." />
-          <select className={`input ${styles.genreSelect}`}>
-            <option value="">All genres</option>
-            <option>Action</option>
-            <option>Drama</option>
-            <option>Comedy</option>
-            <option>Thriller</option>
-          </select>
-        </div> */}
       </div>
+      
       <div className="movie-grid">
         {movies.length === 0 && <p className={styles.emptyMessage}>No movies added yet!</p>}
         {movies.map((movie) => (
